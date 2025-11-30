e465e1bed5727e4230d6ec18a361940f15608faa
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 21:13:28 +0500

    1) Реализовал IBookService. BookService в качестве конструктора принимает репозиторий book, category, status для правильного использования данных
    2) Реализовал динамическое поведение объекта с помощью паттерна состояния, который работает в зависимости от внутреннего состояния объекта, то есть статуса

---
+++ Library_Mvc/Services/BookService.cs
@@ -0,0 +1,123 @@
+using Library_Mvc.Enum;
+using Library_Mvc.Interfaces;
+using Library_Mvc.Models;
+using Library_Mvc.Models.DtoModels;
+using Library_Mvc.PatternState.ContextModel;
+
+namespace Library_Mvc.Services;
+
+public class BookService(IBookRepository bookRepository, ICategoryRepositories categoryRepository, IStatusBookRepository statusBookRepository) : IBookService
+{
+    public async Task<IEnumerable<BookCardDto>> GetAllBooksAsync()
+    {
+        var books = await bookRepository.GetAll();
+        return books.Select(b => new BookCardDto
+        {
+            Author = b.Author,
+            Name = b.Name,
+            RefImg = b.RefImg
+        }).ToList();
+    }
+
+    public async Task<BookDetailDto?> GetBookByIdAsync(long id)
+    {
+        var book = await bookRepository.GetById(id);
+        if (book == null)
+            throw new Exception("Книга не найдена");
+        var category = await categoryRepository.GetById(book.Id);
+        var status = await statusBookRepository.GetById(book.Id);
+        var bookDetailDto = new BookDetailDto
+        {
+            Id = book.Id,
+            Name = book.Name,
+            Author = book.Author,
+            RefImg = book.RefImg,
+            Description = book.Description,
+            CreateDate = book.CreateDate,
+            CategoryId = book.CategoryId,
+            CategoryName = category?.Name ?? "Неизвестная категория",
+            StatusStr = status.Status.ToString(),
+            StatusInt = status.Status,
+            DateAdded = book.DateAdded,
+        };
+
+        return bookDetailDto;
+    }
+
+    public async Task<BookStatusDto> AddBookAsync(BookAddDto bookAddDto)
+    {
+        var category = await categoryRepository.GetByName(bookAddDto.CategoryName);
+
+        if (category == null)
+            throw new Exception("Категория не найдена");
+        
+        var book = new Book
+        {
+            Name = bookAddDto.Name,
+            Author = bookAddDto.Author,
+            RefImg = bookAddDto.RefImg,
+            CreateDate = bookAddDto.CreateDate,
+            Description = bookAddDto.Description,
+            CategoryId = category.Id,
+            DateAdded = DateTime.Now
+        };
+        
+        var id = await bookRepository.Add(book);
+        var statusBook = new StatusBook
+        {
+            BookId = id,
+            Status = Statuses.InStock
+        };
+
+        await statusBookRepository.Add(statusBook);
+        
+        return new BookStatusDto
+        {
+            Name = book.Name,
+            Author = book.Name,
+            RefImg = book.RefImg,
+            Status = "InStock"
+        };
+    }
+
+    public async Task<BookStatusDto> IssuedBook(long id)
+    {
+        var book = await bookRepository.GetById(id);
+        if (book == null)
+            throw new Exception("Книга не найдена");
+        
+        var status = await statusBookRepository.GetById(id);
+        var context = new BookContext(status);
+        context.Issued();
+        await statusBookRepository.Update(context.CurrentBook);
+        return new BookStatusDto
+        {
+            Author = book.Author,
+            Name = book.Name,
+            RefImg = book.RefImg,
+            Status = context.CurrentBook.Status.ToString()
+        };
+    }
+
+    public async Task<BookStatusDto> ReturnBookAsync(long id)
+    {
+        var book = await bookRepository.GetById(id);
+        if (book == null)
+            throw new Exception("Книга не найдена");
+
+        var status = await statusBookRepository.GetById(id);
+        
+        var context = new BookContext(status);
+        context.Issued();
+        return new BookStatusDto
+        {
+            Author = book.Author,
+            Name = book.Name,
+            RefImg = book.RefImg,
+            Status = context.CurrentBook.Status.ToString()
+        };
+    }
+
+    public async Task Delete(long id) => 
+        await bookRepository.Delete(id);
+}
