912f1370bf16e4b182b800d5975a757908c5839f
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 21:16:00 +0500

    1) Переименовал на имя

--- Library_Mvc/Models/StatusBook.cs
+++ Library_Mvc/Models/StatusBook.cs
@@ -2,9 +2,9 @@ using Library_Mvc.Enum;
 
 namespace Library_Mvc.Models;
 
-public class StatusesBooks
+public class StatusBook
 {
     public long Id { get; set; }
-    public long IdBook { get; set; }
+    public long BookId { get; set; }
     public Statuses Status { get; set; } 
 }
