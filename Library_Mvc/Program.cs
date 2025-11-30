14786332b5751830b94c5ecda9b9303113f37982
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 22:48:36 +0500

    1) Program

--- Library_Mvc/Program.cs
+++ Library_Mvc/Program.cs
@@ -1,27 +1,31 @@
 using System.Data;
+using Library_Mvc.Interfaces;
+using Library_Mvc.Repositories;
+using Library_Mvc.Services;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IDbConnection, NpgsqlConnection>(
-    _ => new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IDbConnection>(sp =>
+    new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<ICategoryRepositories, CategoryRepository>();
+builder.Services.AddScoped<IStatusBookRepository, StatusBookRepository>();
+builder.Services.AddScoped<ILibraryUserRepository, LibraryUserRepository>();
+
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+app.UseStaticFiles();
 
-
-
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Library}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
