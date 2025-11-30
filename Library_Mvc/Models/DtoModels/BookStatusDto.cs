7eb1e005e31bbca86111e5dbef1e55317b9f86ff
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 15:20:31 +0500

    1) Изменил тип данных для дальнейшего вывода данных наружу

--- Library_Mvc/Models/DtoModels/BookStatusDto.cs
+++ Library_Mvc/Models/DtoModels/BookStatusDto.cs
@@ -1,10 +1,9 @@
-using Library_Mvc.Enum;
-
 namespace Library_Mvc.Models.DtoModels;
 
 public class BookStatusDto
 {
     public required string Name { get; set; }
     public required string Author { get; set; }
-    public Statuses Status { get; set; }
+    public required string RefImg { get; set; }
+    public required string Status { get; set; }
 }
