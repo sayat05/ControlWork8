705143133d123664f0f3f02b7995449a4e264a65
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 21:14:10 +0500

    1) Определили сервис и его значение

--- Library_Mvc/Interfaces/IBookService.cs
+++ Library_Mvc/Interfaces/IBookService.cs
@@ -1,12 +1,24 @@
+using Library_Mvc.Models.DtoModels;
+
 namespace Library_Mvc.Interfaces;
 
 public interface IBookService
 {
     /*
      Сервис должен уметь:
-     Показывать все книги.
-     Добавлять книгу.
-     Получать книгу.  
-     Менять статус при выдаче и сдаче книги.
+     1 - показывать все книги,
+     2 - показывать книгу по id,
+     3 - добавлять книгу,
+     4 - получать книгу, 
+     5 - менять статус при выдаче и сдаче книги:
+         a - выдать книгу,
+         b - сдать книгу
     */
+
+    Task<IEnumerable<BookCardDto>> GetAllBooksAsync();
+    Task<BookDetailDto?> GetBookByIdAsync(long id);
+    Task<BookStatusDto> AddBookAsync(BookAddDto bookAddDto);
+    Task<BookStatusDto> IssuedBook(long id);
+    Task<BookStatusDto> ReturnBookAsync(long id);
+    Task Delete(long id);
 }
