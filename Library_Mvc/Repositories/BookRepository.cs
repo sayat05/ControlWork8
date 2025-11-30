e876fa8366744e5933dfc7eafcab5e0c678effff
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-11-30 21:08:53 +0500

    1) Полностью реализовал интерфейс IBookRepository работая только с основной моделью

--- Library_Mvc/Repositories/BookRepository.cs
+++ Library_Mvc/Repositories/BookRepository.cs
@@ -27,21 +27,77 @@ public class BookRepository(IDbConnection connection) : IBookRepository
 
     public Task<Book?> GetById(long id)
     {
-        throw new NotImplementedException();
+        var sql = """
+                   select 
+                       b.id, 
+                       b.name, 
+                       b.author, 
+                       b.ref_img as refImg, 
+                       b.create_date as createDate, 
+                       b.description,   
+                       b.date_added as dateAdded, 
+                       b.category_id as categoryId
+                   from books as b
+                   left join categories as c on b.category_id = c.id
+                   where b.id = @id;
+                   """;
+        return connection.QueryFirstOrDefaultAsync<Book>(sql, new {id});
+
     }
 
-    public Task Add(Book book)
+    public Task<int> Add(Book book)
     {
-        throw new NotImplementedException();
+        var sql = """
+                  insert into books ( 
+                  id,
+                  name, 
+                  author,
+                  ref_img, 
+                  create_date, 
+                  description,
+                  date_added,
+                  category_id) 
+                  values (
+                  @id,
+                  @name, 
+                  @author,
+                  @refImg, 
+                  @createDate, 
+                  @description, 
+                  @dateAdded,
+                  @categoryId)
+                  """;
+        book.DateAdded = DateTime.Now;
+
+        return connection.QuerySingleAsync<int>(sql, new
+        {
+            book.Id,
+            book.Author,
+            book.RefImg,
+            book.CreateDate,
+            book.Description,
+            book.CategoryId
+        });
+
     }
 
-    public Task<Book> Update(StatusesBooks book)
+    public Task<StatusBook> Update(StatusBook book)
     {
-        throw new NotImplementedException();
+        var sql = """
+                  update statuses_books set 
+                  status = @status
+                  where id = @bookId;
+                  """;
+        return connection.QuerySingleAsync<StatusBook>(sql, new
+        {
+            book.BookId,
+            book.Status
+        });
     }
 
     public Task Delete(long id)
     {
-        throw new NotImplementedException();
+        const string sql = "delete from books where id = @id";
+        return connection.ExecuteAsync(sql, new { id });
     }
 }
