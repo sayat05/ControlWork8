9b7c8be5a30639061392c1a95f37c995ab9e7f78
Sayat Raximbekov <saintaccaunt@gmail.com>
2025-12-04 12:33:01 +0500

    1) Добавил новую исправленную таблицу бд

---
+++ Library_Mvc/db_v2.sql
@@ -0,0 +1,35 @@
+create table categories
+(
+    id   serial primary key,
+    name varchar not null
+);
+
+create table users
+(
+    id         serial primary key,
+    first_name varchar        not null,
+    last_name  varchar        not null,
+    email      varchar unique not null,
+    phone      varchar        not null
+);
+
+create table books
+(
+    id              serial primary key,
+    name            varchar   not null,
+    author          varchar   not null,
+    ref_img         varchar   not null,
+    year_of_release timestamp not null,
+    description     varchar,
+    created_at      timestamp not null default current_timestamp,
+    category_id     int       not null references categories (id) on delete cascade
+);
+
+create table borrowed_books
+(
+    id            serial primary key,
+    user_id       int not null references users (id) on delete cascade,
+    book_id       int not null references books (id) on delete cascade,
+    date_taken    timestamp default current_timestamp,
+    date_returned timestamp
+);
